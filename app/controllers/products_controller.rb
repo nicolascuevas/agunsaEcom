cfb41a872939656102b07c1515b37d21ce5bfc66
Nicolas Cuevas <nicolascuevas@gmail.com>
2020-07-06 09:35:59 -0400

    order BY date

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.all.order(updated_at: :desc)
     if params[:sku].present?
       @products = Product.where("product_code like ?", "%"+params[:sku]+"%")
     end
